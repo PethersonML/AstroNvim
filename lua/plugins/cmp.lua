9a57d4cb6a650d0f366b81f8cc5a87fcfea5ddb5
PethersonML <petherson.monteiro@hotmail.com>
2026-04-13 14:50:09 -0300

    Last Version 2026-04-13

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,42 +1,42 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      -- 🔕 NÃO abrir menu automaticamente
-      opts.completion = {
-        autocomplete = false,
-      }
-
-      -- 👻 ghost text continua ativo
-      opts.experimental = {
-        ghost_text = true,
-      }
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-      })
-
-      return opts
-    end,
+    -- opts = function(_, opts)
+    --   local cmp = require("cmp")
+    --   local luasnip = require("luasnip")
+    --
+    --   -- 🔕 NÃO abrir menu automaticamente
+    --   opts.completion = {
+    --     autocomplete = false,
+    --   }
+    --
+    --   -- 👻 ghost text continua ativo
+    --   opts.experimental = {
+    --     ghost_text = true,
+    --   }
+    --
+    --   opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
+    --     ["<Tab>"] = cmp.mapping(function(fallback)
+    --       if luasnip.expand_or_jumpable() then
+    --         luasnip.expand_or_jump()
+    --       else
+    --         fallback()
+    --       end
+    --     end, { "i", "s" }),
+    --
+    --     ["<S-Tab>"] = cmp.mapping(function(fallback)
+    --       if luasnip.jumpable(-1) then
+    --         luasnip.jump(-1)
+    --       else
+    --         fallback()
+    --       end
+    --     end, { "i", "s" }),
+    --
+    --     ["<C-Space>"] = cmp.mapping.complete(),
+    --     ["<C-e>"] = cmp.mapping.abort(),
+    --   })
+    --
+    --   return opts
+    -- end,
   }
 }
